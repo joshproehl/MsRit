45190f2309fb007b30c20e4c345136cac4ebce2f
Josh Proehl <josh@daedalusdreams.com>
2015-03-24 11:13:33 -0700

    Make entities parcelable.

--- app/src/main/java/com/daedalusdreams/msrit/entities/Timer.java
+++ app/src/main/java/com/daedalusdreams/msrit/entities/Timer.java
@@ -1,6 +1,8 @@
 package com.daedalusdreams.msrit.entities;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
@@ -8,7 +10,7 @@ import java.util.ArrayList;
  *
  * Created by joshproehl on 3/23/15.
  */
-public class Timer implements Serializable {
+public class Timer implements Parcelable {
     private ArrayList<TimerEvent> events;
     private String name;
 
@@ -36,6 +38,34 @@ public class Timer implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    //endregion
+    //region Implementing Parcelable
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(name);
+        out.writeTypedList(events);
+    }
+
+    public static final Parcelable.Creator<Timer> CREATOR
+            = new Parcelable.Creator<Timer>() {
+        public Timer createFromParcel(Parcel in) {
+            return new Timer(in);
+        }
+
+        public Timer[] newArray(int size) {
+            return new Timer[size];
+        }
+    };
+
+    private Timer(Parcel in) {
+        name = in.readString();
+        events = in.createTypedArrayList(TimerEvent.CREATOR);
     }
 
     //endregion
--- app/src/main/java/com/daedalusdreams/msrit/entities/TimerEvent.java
+++ app/src/main/java/com/daedalusdreams/msrit/entities/TimerEvent.java
@@ -1,13 +1,15 @@
 package com.daedalusdreams.msrit.entities;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 
 /**
  * Represents a specific TimerEvent, one or more of which make up a Timer. (TimerEvents are what contain the actual countdown length information.)
  *
  * Created by joshproehl on 3/23/15.
  */
-public class TimerEvent implements Serializable {
+public class TimerEvent implements Parcelable {
     private int seconds;
 
     public TimerEvent(int seconds) {
@@ -22,6 +24,32 @@ public class TimerEvent implements Serializable {
 
     public void setSeconds(int seconds) {
         this.seconds = seconds;
+    }
+
+    //endregion
+    //region Implementing Parcelable
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(seconds);
+    }
+
+    public static final Parcelable.Creator<TimerEvent> CREATOR
+            = new Parcelable.Creator<TimerEvent>() {
+        public TimerEvent createFromParcel(Parcel in) {
+            return new TimerEvent(in);
+        }
+
+        public TimerEvent[] newArray(int size) {
+            return new TimerEvent[size];
+        }
+    };
+
+    private TimerEvent(Parcel in) {
+        seconds = in.readInt();
     }
 
     //endregion
