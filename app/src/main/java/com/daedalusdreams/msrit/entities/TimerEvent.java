1c96bd0d115826e3566263eb52fffcefc4644a53
Josh Proehl <josh@daedalusdreams.com>
2015-03-24 13:31:50 -0700

    Add flux into timerEvent

--- app/src/main/java/com/daedalusdreams/msrit/entities/TimerEvent.java
+++ app/src/main/java/com/daedalusdreams/msrit/entities/TimerEvent.java
@@ -7,10 +7,17 @@ import android.os.Parcelable;
 /**
  * Represents a specific TimerEvent, one or more of which make up a Timer. (TimerEvents are what contain the actual countdown length information.)
  *
+ * Each timer event has a target number of seconds, and a random variability range.
+ * For example, an event may be 60 seconds long, with 10 seconds of variability, which means it will
+ * run for somewhere between 50 and 70 seconds.)
+ *
  * Created by joshproehl on 3/23/15.
  */
 public class TimerEvent implements Parcelable {
+    // How long is this specific event going to last.
     private int seconds;
+    // The number of seconds that we could be before or after the target seconds.
+    private int secondsFlux;
 
     public TimerEvent(int seconds) {
         this.seconds = seconds;
@@ -26,6 +33,14 @@ public class TimerEvent implements Parcelable {
         this.seconds = seconds;
     }
 
+    public int getSecondsFlux() {
+        return secondsFlux;
+    }
+
+    public void setSecondsFlux(int secondsFlux) {
+        this.secondsFlux = secondsFlux;
+    }
+
     //endregion
     //region Implementing Parcelable
 
@@ -35,6 +50,7 @@ public class TimerEvent implements Parcelable {
 
     public void writeToParcel(Parcel out, int flags) {
         out.writeInt(seconds);
+        out.writeInt(secondsFlux);
     }
 
     public static final Parcelable.Creator<TimerEvent> CREATOR
@@ -50,6 +66,7 @@ public class TimerEvent implements Parcelable {
 
     private TimerEvent(Parcel in) {
         seconds = in.readInt();
+        secondsFlux = in.readInt();
     }
 
     //endregion
